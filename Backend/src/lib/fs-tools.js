f8c91732fa9f559e1a79d8ef4a5ece371d32c77a
Leonardo Vezzati <leonardo.vezzati@gmail.com>
2021-05-21 12:09:57 +0200

    Commit Upload Image in the lib

--- Backend/src/lib/fs-tools.js
+++ Backend/src/lib/fs-tools.js
@@ -9,26 +9,28 @@ const { readJSON, writeJSON, writeFile, createReadStream } = fs;
 
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
 
+const publicImgFolderPath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "../../public/img"
+);
+
 export const getProducts = async () =>
   await readJSON(join(dataFolderPath, "products.json"));
-
-export const getReviews = async () =>
-  await readJSON(join(dataFolderPath, "reviews.json"));
 
 export const writeProducts = async (content) =>
   await writeJSON(join(dataFolderPath, "products.json"), content);
 
+export const getReviews = async () =>
+  await readJSON(join(dataFolderPath, "reviews.json"));
+
 export const writeReviews = async (content) =>
   await writeJSON(join(dataFolderPath, "reviews.json"), content);
-
-/* export const writeStudentsPictures = async (fileName, content) =>
-  await writeFile(join(studentsFolderPath, fileName), content);
- */
 
 export const getCurrentFolderPath = (currentFile) =>
   dirname(fileURLToPath(currentFile));
 
-/* 
-export const readStudentsPictures = (fileName) =>
-  createReadStream(join(studentsFolderPath, fileName));
- */
+export const writeProductsPictures = async (fileName, content) =>
+  await writeFile(join(publicImgFolderPath, fileName), content);
+
+export const readProductsPictures = (fileName) =>
+  createReadStream(join(publicImgFolderPath, fileName));
