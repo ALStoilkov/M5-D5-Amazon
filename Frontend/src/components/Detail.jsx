a1d844784ab9820e79d9e653452904b7c214ea43
MSajedian <mohammadsajedian@gmail.com>
2021-05-21 16:56:49 +0100

    fixes

--- Frontend/src/components/Detail.jsx
+++ Frontend/src/components/Detail.jsx
@@ -1,7 +1,5 @@
-import { Button } from "bootstrap";
 import React, { Component } from "react";
 import fetchProduct from "../services/fetchProduct";
-import UploadImgModal from "./UploadImgModal";
 
 class Detail extends Component {
   constructor(props) {
@@ -19,54 +17,46 @@ class Detail extends Component {
   };
 
 
-    render() {
-        return (<div className="container">
-            <div className="row">
-                <h1 className="text-center my-3">Products Detail</h1>
-                <div className="col-10 col-md-6 mx-auto">
-                    {this.state.product && (
-                        <div>
-                            <div className="d-flex justify-content-center">
-                                <img id="imageUrl" style={{ maxHeight: 200 }} alt="" src={this.state.product.imageUrl} />
-                            </div>
-                            <div>
-                                <div><strong>Name:</strong></div>
-                                <p id="name" >{this.state.product.name}</p>
-                            </div>
-                            <div>
-                                <div><strong>Description:</strong></div>
-                                <p id="description" >{this.state.product.description}</p>
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <h1 className="text-center my-3">Products Detail</h1>
+          <div className="col-10 col-md-6 mx-auto">
+            {this.state.product && (
+              <div>
+                <div className="d-flex justify-content-center">
+                  <img id="imageUrl" style={{ maxHeight: 200 }} alt="" src={this.state.product.imageUrl} />
+                </div>
+                <div>
+                  <div><strong>Name:</strong></div>
+                  <p id="name" >{this.state.product.name}</p>
+                </div>
+                <div>
+                  <div><strong>Description:</strong></div>
+                  <p id="description" >{this.state.product.description}</p>
 
-                            </div>
-                            <div>
-                                <div>
-                                    <strong>Brand:</strong>
-                                    <p id="brand" >{this.state.product.brand}</p>
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <strong>Price:</strong>
-                                    <div id="price" >{this.state.product.price}</div>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                    <a href={"/backoffice/" + this.state.product._id} className="btn text-center" >Edit this product</a>
+                </div>
+                <div>
+                  <div>
+                    <strong>Brand:</strong>
+                    <p id="brand" >{this.state.product.brand}</p>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <strong>Price:</strong>
+                    <div id="price" >{this.state.product.price}</div>
+                  </div>
                 </div>
               </div>
             )}
-            <a
-              href={"/backoffice?id=" + this.state.product._id}
-              className="btn text-center"
-            >
-              Edit this product
-            </a>
-            <UploadImgModal {...this.props} />
+            <a href={"/backoffice/" + this.state.product._id} className="btn text-center" >Edit this product</a>
           </div>
         </div>
+        <a href={"/backoffice?id=" + this.state.product._id} className="btn text-center" > Edit this product </a>
       </div>
-    );
+    )
   }
 }
 
